cd6d7611aa3c42c46b0059218e26507d06e33625
Samuel Jedidiah Uy <samueluy@Samuels-Laptop.local>
2023-12-04 11:41:14 +0800

    modified Main to fix output

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import threading
 from FittingRoom import FittingRoom
 
-
-# For quicker testing
-# n = 5
-# b = 3
-# g = 3
-
 # User input
 n = int(input("Enter number of slots inside the fitting room: "))
 b = int(input("Enter number of blue threads: "))
 g = int(input("Enter number of green threads: "))
 
-fitting_room = FittingRoom(n) # Create a fitting room object
-threads = [] # Create a list of threads
+fitting_room = FittingRoom(n)  # Create a fitting room object
+enter_threads = []  # Create a list of entering threads
+leave_threads = []  # Create a list of leaving threads
+
 print("**************************************\nProgram Start\n**************************************")
 
-# Create blue threads
+# Create blue entering threads
 for i in range(b):
-    threads.append(threading.Thread(target = fitting_room.enter, args = ('b',)))
-    threads.append(threading.Thread(target = fitting_room.leave, args = ('b',)))
+    enter_threads.append(threading.Thread(target=fitting_room.enter, args=('b',)))
 
-# Create green threads
+# Create green entering threads
 for i in range(g):
-    threads.append(threading.Thread(target = fitting_room.enter, args = ('g',)))
-    threads.append(threading.Thread(target = fitting_room.leave, args = ('g',)))
+    enter_threads.append(threading.Thread(target=fitting_room.enter, args=('g',)))
 
-# Start all threads
-for thread in threads:
+# Start entering threads
+for thread in enter_threads:
     thread.start()
 
-# Wait for all threads to terminate
-for thread in threads:
+# Wait for all entering threads to terminate
+for thread in enter_threads:
+    thread.join()
+
+# Create blue leaving threads
+for i in range(b):
+    leave_threads.append(threading.Thread(target=fitting_room.leave, args=('b',)))
+
+# Create green leaving threads
+for i in range(g):
+    leave_threads.append(threading.Thread(target=fitting_room.leave, args=('g',)))
+
+# Start leaving threads
+for thread in leave_threads:
+    thread.start()
+
+# Wait for all leaving threads to terminate
+for thread in leave_threads:
     thread.join()
 
 print("**************************************\nProgram End\n**************************************")
