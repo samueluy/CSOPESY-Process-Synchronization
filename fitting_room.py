85fefa54ae49712b932b0f27fcacadeb93d26358
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-12-05 03:08:33 +0800

    added comments

--- fitting_room.py
+++ fitting_room.py
@@ -5,23 +5,24 @@ logging.basicConfig(level=logging.DEBUG, format='%(message)s',)
 
 class FittingRoom:
     def __init__(self, n):
-        self.slots = n
+        self.slots = n  # Number of slots in the fitting room.
         self.lock = threading.Lock()
-        self.condition = threading.Condition(self.lock)
-        self.current_color = None
-        self.occupancy = 0
-        self.waiting = {'blue': 0, 'green': 0}
+        self.condition = threading.Condition(self.lock) 
+        self.current_color = None  # Tracks the color of the threads currently using the fitting room.
+        self.occupancy = 0  # Tracks the current number of threads in the fitting room.
+        self.waiting = {'blue': 0, 'green': 0}  # Tracks the number of waiting threads for each color.
 
     def enter(self, color):
         thread_id = threading.current_thread().name
         with self.condition:
             self.waiting[color] += 1
+            # Wait if the fitting room is occupied by the other color or is full.
             while (self.current_color is not None and self.current_color != color) or \
                   (self.current_color == color and self.occupancy == self.slots):
                 self.condition.wait()
             self.waiting[color] -= 1
             self.occupancy += 1
-            if self.occupancy == 1:
+            if self.occupancy >= 1:
                 logging.debug(f"\n{color.capitalize()} only.")
             self.current_color = color
             logging.debug(f"{thread_id} entered the fitting room.")
@@ -30,7 +31,7 @@ class FittingRoom:
         thread_id = threading.current_thread().name
         with self.condition:
             self.occupancy -= 1
-            if self.occupancy == 0:
+            if self.occupancy <= 0:
                 self.switch_turn()
                 logging.debug("\nEmpty fitting room.")
 
@@ -38,8 +39,8 @@ class FittingRoom:
         next_color = 'green' if self.current_color == 'blue' else 'blue'
         if self.waiting[next_color] > 0:
             self.current_color = next_color
-            self.condition.notify_all()
+            self.condition.notify_all() # Notify all waiting threads of the next color.
         elif self.waiting[self.current_color] > 0:
-            self.condition.notify_all()
+            self.condition.notify_all() # If no waiting threads of the next color, notify all waiting threads of the current color.
         else:
             self.current_color = None
